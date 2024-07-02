ae74bf5d8bf3bfa5fd68d3ca8d5484c03ef257ab
FuuNick <93815279+FuuNick@users.noreply.github.com>
2024-07-03 05:22:22 +0700

    Second

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+        fetch('https://yesir-server.vercel.app/hello')
+            .then(res => res.json())
+            .then(data => setMessage(data.message))
+            .catch(err => console.error('Error fetching message:', err));
+    }, []);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>{message}</h1>
+            </header>
+        </div>
+    );
 }
 
 export default App;
